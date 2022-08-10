327ffc4bfbbd458eba4d5274336e45dc9d00fd41
sijiahu <scarlettshcode@gmail.com>
2022-08-10 18:26:26 -0500

    simply store username and password

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(
+  "mongodb+srv://admin-scarlett:" +
+    process.env.mongodbPass +
+    "@cluster0.y3z9a.mongodb.net/userDB"
+);
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = mongoose.model("User", userSchema);
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({
+    email: email,
+    password: password,
+  });
+
+  User.insertMany(user, (err) => {
+    if (!err) {
+      res.send("Sueccesfully registered");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: email }, (err, foundResult) => {
+    if (err) {
+      console.log(err);
+    } else if (foundResult.password === password) {
+      res.render("secrets");
+    }
+  });
+});
+
+app.get("/logout", (req, res) => {});
+
+app.get("/submit", (req, res) => {});
+
+app.listen(3000, (req, res) => {
+  console.log("Server started");
+});
